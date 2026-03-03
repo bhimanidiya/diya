881e790fbb607a5e3acc18ae9b2a11d55aa564f6
bhimanidiya <bhimanidiya04@gmail.com>
2026-03-03 08:05:15 +0530

    Add files via upload

---
+++ whileloop2.py
@@ -0,0 +1,14 @@
+# sum of first n natural numbers 
+n=int(input("enter n:"))
+i=1
+s=0
+while i<=n:
+    s=s+1
+    i=i+1
+print("sum=",s)
+# table of a number 
+num=int (input("enter no :"))
+i=1
+while i<=10:
+    print (num , "x" , i , "=" , num*1)
+    i=i+1
